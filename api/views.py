4449bed88f8a1a4e2345a5ef42903529de4e9341
Esteban Garcia <egarcia@cloudsmith.io>
2025-07-08 19:51:46 +0100

    feat: added new endpoint for book creation

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import BookSerializer
+from .models import Book
 
 class HealthView(APIView):
     def get(self, request, *args, **kwargs):
@@ -9,12 +11,24 @@ class HealthView(APIView):
 
 health_view = HealthView.as_view()
 
+#
+# /api/books - All methods (GET, POST)
+#
 class BookView(APIView):
     """ List all books, or create a new book """
     
     def get(self, request, *args, **kwargs):
-        return Response({
-            "hello": "django"
-        })
+        all_books = Book.objects.all()
+        serializer = BookSerializer(all_books, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        
+        serializer = BookSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
+        return Response(serializer.data)
+        
 book_view = BookView.as_view()
